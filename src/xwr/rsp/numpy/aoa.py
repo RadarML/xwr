6573d04ea446231d891e6d8ecf9fa151184fcfec
huangjuite <huangjuite@gmail.com>
2026-08-30 17:17:15 -0400

    numpy: build angle tables from the cube shape
    Derives the bin-to-angle lookups from the cube's own angle axes inside
    `_point_cloud`, rather than reading them off the instance.

--- src/xwr/rsp/numpy/aoa.py
+++ src/xwr/rsp/numpy/aoa.py
@@ -9,8 +9,9 @@ from xwr.rsp import aoa as base
 class PointCloud(base.PointCloud[np.ndarray]):
     """Get radar point cloud from post FFT cube."""
 
-    @staticmethod
-    def _asarray(x: Float[np.ndarray, "..."]) -> Float[np.ndarray, "..."]:
+    def _asarray(
+        self, x: Float[np.ndarray, "..."], like: Float32[np.ndarray, "..."]
+    ) -> Float[np.ndarray, "..."]:
         return x
 
     @staticmethod
@@ -29,14 +30,17 @@ class PointCloud(base.PointCloud[np.ndarray]):
         Bool[np.ndarray, "batch range doppler"],
         Float32[np.ndarray, "batch range doppler 4"],
     ]:
+        el_angles = self._asarray(self.angle_table(cube.shape[2]), cube)
+        az_angles = self._asarray(self.angle_table(cube.shape[3]), cube)
+
         _, r_size, d_size = mask.shape
         range_v = np.arange(r_size) * self.range_res
         doppler_v = (np.arange(d_size) - d_size // 2) * self.doppler_res
         r_grid, d_grid = np.meshgrid(range_v, doppler_v, indexing="ij")
 
         angle_idx = self.aoa(cube.transpose(0, 4, 1, 2, 3))
-        ang_e = self.el_angles[angle_idx[..., 0]]
-        ang_a = self.az_angles[angle_idx[..., 1]]
+        ang_e = el_angles[angle_idx[..., 0]]
+        ang_a = az_angles[angle_idx[..., 1]]
         mask_e = np.logical_and(ang_e < self.el_fov, ang_e > -self.el_fov)
         mask_a = np.logical_and(ang_a < self.az_fov, ang_a > -self.az_fov)
         mask_ang = np.logical_and(mask_a, mask_e)
